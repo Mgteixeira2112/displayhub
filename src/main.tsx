39e6adaa966939cdecec85b7a293fcde25c0a369
Mgteixeira2112 <102971119+Mgteixeira2112@users.noreply.github.com>
2026-09-09 07:48:45 -0300

    feat: load phase 5 playlist styles

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App'
 import './styles.css'
 import './structured.css'
+import './playlists.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
