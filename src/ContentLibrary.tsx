fbd59c35c9ee1a33cf256a03d41dab313b689d88
Mgteixeira2112 <102971119+Mgteixeira2112@users.noreply.github.com>
2026-09-10 21:36:15 -0300

    fix: recarregar módulos após qualquer alteração de dados

--- src/ContentLibrary.tsx
+++ src/ContentLibrary.tsx
@@ -1,9 +1,11 @@
+import { useEffect, useRef, useState } from 'react'
 import ContentLibraryCore from './ContentLibraryCore'
 import MonitoringPanel from './MonitoringPanel'
 import PlaylistManager from './PlaylistManager'
 import PromotionPosterManager from './PromotionPosterManager'
 import StructuredContent from './StructuredContent'
 import TemplateManager from './TemplateManager'
+import { subscribeDataChanged } from './lib/dataRefresh'
 
 type Props = {
   companyId: string
@@ -11,31 +13,51 @@ type Props = {
 }
 
 export default function ContentLibrary(props: Props) {
+  const [dataVersion, setDataVersion] = useState(0)
+  const refreshTimerRef = useRef<number | null>(null)
+
+  useEffect(() => {
+    const unsubscribe = subscribeDataChanged(() => {
+      if (refreshTimerRef.current !== null) window.clearTimeout(refreshTimerRef.current)
+      refreshTimerRef.current = window.setTimeout(() => {
+        setDataVersion((current) => current + 1)
+        refreshTimerRef.current = null
+      }, 120)
+    })
+
+    return () => {
+      unsubscribe()
+      if (refreshTimerRef.current !== null) window.clearTimeout(refreshTimerRef.current)
+    }
+  }, [])
+
   return (
     <>
       <div className="software-module module-overview">
         <MonitoringPanel companyId={props.companyId} />
       </div>
 
-      <div className="software-module module-library">
-        <ContentLibraryCore {...props} />
-        <StructuredContent {...props} />
-      </div>
+      <div key={dataVersion}>
+        <div className="software-module module-library">
+          <ContentLibraryCore {...props} />
+          <StructuredContent {...props} />
+        </div>
 
-      <div className="software-module module-posters">
-        <PromotionPosterManager {...props} />
-      </div>
+        <div className="software-module module-posters">
+          <PromotionPosterManager {...props} />
+        </div>
 
-      <div className="software-module module-playlists">
-        <PlaylistManager {...props} />
-      </div>
+        <div className="software-module module-playlists">
+          <PlaylistManager {...props} />
+        </div>
 
-      <div className="software-module module-schedule">
-        <PlaylistManager {...props} />
-      </div>
+        <div className="software-module module-schedule">
+          <PlaylistManager {...props} />
+        </div>
 
-      <div className="software-module module-templates">
-        <TemplateManager {...props} />
+        <div className="software-module module-templates">
+          <TemplateManager {...props} />
+        </div>
       </div>
 
       <div className="software-module module-history">
