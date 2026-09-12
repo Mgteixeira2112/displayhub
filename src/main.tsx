a2b7270ab1395afc85799d83919aad618e024b49
Mgteixeira2112 <102971119+Mgteixeira2112@users.noreply.github.com>
2026-09-12 12:29:01 -0300

    ux: carregar cabeçalho compacto do Criar

--- src/main.tsx
+++ src/main.tsx
@@ -30,6 +30,7 @@ import './promotion-editor-tools.css'
 import './promotion-creator-workspace.css'
 import './promotion-creator-content-panel.css'
 import './promotion-creator-compact.css'
+import './promotion-creator-header-compact.css'
 
 function getPublicToken() {
   const basePath = import.meta.env.BASE_URL.replace(/\/$/, '')
