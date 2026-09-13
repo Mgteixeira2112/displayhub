da4b5ca45e77b6efcd6b2a6d2214a9922aa018d0
Mgteixeira2112 <102971119+Mgteixeira2112@users.noreply.github.com>
2026-09-13 18:14:01 -0300

    feat: expor pareamento seguro para a tela de configuração

--- windows-player/preload.cjs
+++ windows-player/preload.cjs
@@ -5,6 +5,8 @@ contextBridge.exposeInMainWorld('displayHubPlayer', {
   refreshMonitors: () => ipcRenderer.invoke('player:refresh-monitors'),
   saveMappings: (mappings, settings) => ipcRenderer.invoke('player:save-mappings', mappings, settings),
   reset: () => ipcRenderer.invoke('player:reset'),
+  startPairing: () => ipcRenderer.invoke('player:start-pairing'),
+  pollPairing: (pairingId) => ipcRenderer.invoke('player:poll-pairing', pairingId),
   onMonitorsChanged: (callback) => {
     ipcRenderer.removeAllListeners('player:monitors-changed')
     ipcRenderer.on('player:monitors-changed', () => callback())
